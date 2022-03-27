7b9bf18bc0ebe5f7b5bb9fed6111e593d67b9195
Eun <43868540+JegalEun@users.noreply.github.com>
2022-03-28 00:02:33 +0900

    결제페이지 레이아웃 코인애니메이션 추가

--- app/src/main/java/com/example/risingtest/src/deliveryToBuy/DeliveryToBuyActivity.kt
+++ app/src/main/java/com/example/risingtest/src/deliveryToBuy/DeliveryToBuyActivity.kt
@@ -1,12 +1,26 @@
 package com.example.risingtest.src.deliveryToBuy
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import com.example.risingtest.config.BaseActivity
 import com.example.risingtest.databinding.ActivityDeliveryToBuyBinding
+import com.github.ybq.android.spinkit.animation.AnimationUtils.start
+
 
 class DeliveryToBuyActivity : BaseActivity<ActivityDeliveryToBuyBinding>(ActivityDeliveryToBuyBinding::inflate) {
 
+    private var animationDrawable: AnimationDrawable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        coinAni()
+    }
+
+    // 코인 돌아가는 애니메이션
+    fun coinAni(){
+
+        animationDrawable = binding.ivAnimationCoin.background as AnimationDrawable
+        animationDrawable!!.start()
     }
 }
