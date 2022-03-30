463986450f6a3e6b514069398eb7175c094de80a
Eun <43868540+JegalEun@users.noreply.github.com>
2022-03-30 13:12:18 +0900

    상품 등록 레이아웃 구현

--- app/src/main/java/com/example/risingtest/src/MainActivity.kt
+++ app/src/main/java/com/example/risingtest/src/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.risingtest.src
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.risingtest.R
@@ -7,7 +8,9 @@ import com.example.risingtest.config.BaseActivity
 import com.example.risingtest.databinding.ActivityMainBinding
 import com.example.risingtest.src.main.home.HomeFragment
 import com.example.risingtest.src.main.myPage.MyPageFragment
+import com.example.risingtest.src.main.register.RegisterProductActivity
 import com.example.risingtest.src.main.talk.TalkFragment
+import com.example.risingtest.src.register.RegisterActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
@@ -37,6 +40,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
                             .replace(R.id.main_frm, MyPageFragment())
                             .commitAllowingStateLoss()
                         return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.menu_main_btm_nav_register -> {
+                        val intent = Intent(this, RegisterProductActivity::class.java)
+                        startActivity(intent)
                     }
                 }
                 false
---
+++ app/src/main/java/com/example/risingtest/src/main/register/RegisterProductActivity.kt
@@ -0,0 +1,13 @@
+package com.example.risingtest.src.main.register
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.risingtest.config.BaseActivity
+import com.example.risingtest.databinding.ActivityProductRegisterBinding
+
+class RegisterProductActivity : BaseActivity<ActivityProductRegisterBinding>(ActivityProductRegisterBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
